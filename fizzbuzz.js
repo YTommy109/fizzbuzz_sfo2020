5a95509b3ca730000442a30558c056a932c36c86
Tommy109 <tokutomi@degino.com>
2020-06-28 19:56:02 +0900

    引数を受け取るようにした。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,8 +2,8 @@ const fizz3 = v => v%3?'':'Fizz'
 const buzz5 = v => v%5?'':'Buzz'
 const fizzbuzz = v => fizz3(v)+buzz5(v) || `${v}`
 
-const main = () => {
-    return [...Array(15).keys()].map(it => fizzbuzz(it+1))
+const main = (max=15) => {
+    return [...Array(max).keys()].map(it => fizzbuzz(it+1))
 }
 
 
