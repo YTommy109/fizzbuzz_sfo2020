f75a964042b3f36b2a1b5b48d5d58dd20191554e
Tommy109 <tokutomi@degino.com>
2020-06-28 19:48:45 +0900

    配列を map に置き換えた。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,3 @@
-// TODO: Map にする
-
 const fizzbuzz = v => {
     if (v%15===0) return 'FizzBuzz'
     if (v%3===0) return 'Fizz'
@@ -8,11 +6,7 @@ const fizzbuzz = v => {
 }
 
 const main = () => {
-    return [
-        fizzbuzz(1), fizzbuzz(2), fizzbuzz(3), fizzbuzz(4), fizzbuzz(5),
-        fizzbuzz(6), fizzbuzz(7), fizzbuzz(8), fizzbuzz(9), fizzbuzz(10),
-        fizzbuzz(11), fizzbuzz(12), fizzbuzz(13), fizzbuzz(14), fizzbuzz(15)
-    ]
+    return [...Array(15).keys()].map(it => fizzbuzz(it+1))
 }
 
 
