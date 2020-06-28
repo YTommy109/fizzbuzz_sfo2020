05c0795a2fe55f2df4063eb0cffddeeddd2d72ba
Tommy109 <tokutomi@degino.com>
2020-06-28 19:43:14 +0900

    Buzz 判定を入れた。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,12 +3,13 @@
 
 const fizzbuzz = v => {
     if (v%3===0) return 'Fizz'
+    if (v%5===0) return 'Buzz'
 }
 
 const main = () => {
     return [
-        '1', '2', fizzbuzz(3), '4', 'Buzz',
-        fizzbuzz(6), '7', '8', fizzbuzz(9), 'Buzz',
+        '1', '2', fizzbuzz(3), '4', fizzbuzz(5),
+        fizzbuzz(6), '7', '8', fizzbuzz(9), fizzbuzz(10),
         '11', fizzbuzz(12), '13', '14', 'FizzBuzz'
     ]
 }
