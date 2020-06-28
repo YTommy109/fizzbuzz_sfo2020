3f3707b6730493451174217b21b88270a742851d
Tommy109 <tokutomi@degino.com>
2020-06-28 19:56:57 +0900

    仮引数のデフォルト値を消した。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,7 @@ const fizz3 = v => v%3?'':'Fizz'
 const buzz5 = v => v%5?'':'Buzz'
 const fizzbuzz = v => fizz3(v)+buzz5(v) || `${v}`
 
-const main = (max=15) => {
+const main = (max) => {
     return [...Array(max).keys()].map(it => fizzbuzz(it+1))
 }
 
--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -7,8 +7,15 @@ describe('FizzBuzz について', () => {
             'Fizz', '7', '8', 'Fizz', 'Buzz',
             '11', 'Fizz', '13', '14', 'FizzBuzz'
         ]
-        const target = main()
+        const target = main(15)
 
         expect(target).toEqual(exp)
+    })
+
+    it('指定したところまで FizzBuzz を作ること', () => {
+        const target = main(20)
+        expect(target).toHaveLength(20)
+        expect(target[20-1]).toEqual('Buzz')
+        expect(target[21-1]).toBeUndefined()
     })
 })
