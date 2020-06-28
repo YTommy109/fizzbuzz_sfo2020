be98f44a925b5900b66795596463cd4090607789
Tommy109 <tokutomi@degino.com>
2020-06-28 19:54:46 +0900

    アロー関数の省略記法に変えた。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,6 @@
 const fizz3 = v => v%3?'':'Fizz'
 const buzz5 = v => v%5?'':'Buzz'
-const fizzbuzz = v => {
-    return fizz3(v)+buzz5(v) || `${v}`
-}
+const fizzbuzz = v => fizz3(v)+buzz5(v) || `${v}`
 
 const main = () => {
     return [...Array(15).keys()].map(it => fizzbuzz(it+1))
