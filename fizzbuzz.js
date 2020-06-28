f65536be8de3211152de54ebf9ac5a62ce649c71
Tommy109 <tokutomi@degino.com>
2020-06-28 19:45:46 +0900

    通常出力を入れた。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,17 @@
-// TODO: fizzbuzz 判定を入れる
 // TODO: Map にする
 
 const fizzbuzz = v => {
     if (v%15===0) return 'FizzBuzz'
     if (v%3===0) return 'Fizz'
     if (v%5===0) return 'Buzz'
+    return `${v}`
 }
 
 const main = () => {
     return [
-        '1', '2', fizzbuzz(3), '4', fizzbuzz(5),
-        fizzbuzz(6), '7', '8', fizzbuzz(9), fizzbuzz(10),
-        '11', fizzbuzz(12), '13', '14', fizzbuzz(15)
+        fizzbuzz(1), fizzbuzz(2), fizzbuzz(3), fizzbuzz(4), fizzbuzz(5),
+        fizzbuzz(6), fizzbuzz(7), fizzbuzz(8), fizzbuzz(9), fizzbuzz(10),
+        fizzbuzz(11), fizzbuzz(12), fizzbuzz(13), fizzbuzz(14), fizzbuzz(15)
     ]
 }
 
