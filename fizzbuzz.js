27f9f55ec3de855bf88a814b0149bbf803fbe952
Tommy109 <tokutomi@degino.com>
2020-06-28 19:53:30 +0900

    Fizz 判定をインライン化した。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,9 +2,8 @@ const fizz3 = v => v%3?'':'Fizz'
 const buzz5 = v => v%5?'':'Buzz'
 const fizzbuzz = v => {
     if (v%15===0) return 'FizzBuzz'
-    if (v%3===0) return fizz3(v)
     if (v%5===0) return buzz5(v)
-    return `${v}`
+    return fizz3(v) || `${v}`
 }
 
 const main = () => {
