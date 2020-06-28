a62c8b88532ddd280852bd1decef03daf5662443
Tommy109 <tokutomi@degino.com>
2020-06-28 19:42:33 +0900

    Fizz 判定を入れた。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,15 @@
 // TODO: fizzbuzz 判定を入れる
 // TODO: Map にする
 
+const fizzbuzz = v => {
+    if (v%3===0) return 'Fizz'
+}
+
 const main = () => {
     return [
-        '1', '2', 'Fizz', '4', 'Buzz',
-        'Fizz', '7', '8', 'Fizz', 'Buzz',
-        '11', 'Fizz', '13', '14', 'FizzBuzz'
+        '1', '2', fizzbuzz(3), '4', 'Buzz',
+        fizzbuzz(6), '7', '8', fizzbuzz(9), 'Buzz',
+        '11', fizzbuzz(12), '13', '14', 'FizzBuzz'
     ]
 }
 
