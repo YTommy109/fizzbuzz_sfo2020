eea18f83464d9b141ed2cef8d8364d9dbef8d1cb
Tommy109 <tokutomi@degino.com>
2020-06-28 19:51:28 +0900

    Buzz 判定を外に出した。

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,9 @@
 const fizz3 = v => v%3?'':'Fizz'
+const buzz5 = v => v%5?'':'Buzz'
 const fizzbuzz = v => {
     if (v%15===0) return 'FizzBuzz'
     if (v%3===0) return fizz3(v)
-    if (v%5===0) return 'Buzz'
+    if (v%5===0) return buzz5(v)
     return `${v}`
 }
 
